d00abf6985dd67f591795c30e60224562c4c39aa
cryptroute <cryptroute@hotmail.com>
2024-11-13 02:26:13 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -558,7 +558,7 @@ fi
 cd xwireguard || exit
 # Install WGDashboard
 echo "Installing WGDashboard ....."
-git clone -q -b v3.1-dev https://github.com/donaldzou/WGDashboard.git wgdashboard
+git clone https://github.com/donaldzou/WGDashboard.git wgdashboard
 cd wgdashboard/src
 #apt install python3-pip -y && pip install gunicorn && pip install -r requirements.txt --ignore-installed
 apt install python3-pip -y >/dev/null 2>&1 && pip install gunicorn >/dev/null 2>&1 && pip install -r requirements.txt --ignore-installed >/dev/null 2>&1
